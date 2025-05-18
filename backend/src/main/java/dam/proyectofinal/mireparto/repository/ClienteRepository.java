f8064ee7453334d2681659efc2b7eb02d405c315
Diego-Humbser <Diego-Humbser1@Eep-Igroup.com>
2025-05-18 16:39:00 +0200

    Declarar ClienteRepository

---
+++ backend/src/main/java/dam/proyectofinal/mireparto/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package dam.proyectofinal.mireparto.repository;
+
+import dam.proyectofinal.mireparto.domain.Cliente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    // findAll(), findById(), save(), deleteById(), etc.
+}
