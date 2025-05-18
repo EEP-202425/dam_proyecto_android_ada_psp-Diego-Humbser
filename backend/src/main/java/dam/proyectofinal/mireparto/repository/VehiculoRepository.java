1fde36d83067012c45ab8c98725b29370367a704
Diego-Humbser <Diego-Humbser1@Eep-Igroup.com>
2025-05-18 16:39:15 +0200

    Declarar VehiculoRepository

---
+++ backend/src/main/java/dam/proyectofinal/mireparto/repository/VehiculoRepository.java
@@ -0,0 +1,9 @@
+package dam.proyectofinal.mireparto.repository;
+
+import dam.proyectofinal.mireparto.domain.Vehiculo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VehiculoRepository extends JpaRepository<Vehiculo, Long> {
+    // findAll(), findById(), save(), deleteById(), etc.
+}
