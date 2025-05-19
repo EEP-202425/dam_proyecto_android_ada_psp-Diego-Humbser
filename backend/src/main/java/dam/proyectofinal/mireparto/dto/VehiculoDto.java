062b6a86d5d98f3c4856e8ad9089a4f4da7968ce
Diego-Humbser <Diego-Humbser1@Eep-Igroup.com>
2025-05-19 13:16:17 +0200

    RT-SEG-01: Añadir anotaciones de Bean Validation a DTOs

--- backend/src/main/java/dam/proyectofinal/mireparto/dto/VehiculoDto.java
+++ backend/src/main/java/dam/proyectofinal/mireparto/dto/VehiculoDto.java
@@ -2,6 +2,11 @@ package dam.proyectofinal.mireparto.dto;
 
 import dam.proyectofinal.mireparto.domain.Vehiculo;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,8 +17,14 @@ import lombok.NoArgsConstructor;
 public class VehiculoDto {
     
 	private Long id;
+	
+    @NotBlank @Size(max = 15)
     private String matricula;
+    
+    @NotBlank @Size(max = 50)
     private String tipo;
+    
+    @NotNull @Positive
     private Double capacidad;
     
     public VehiculoDto(Vehiculo v) {
